afd3401ebe52c5075e85c2369c13e65dd31edf92
Sergei <sergei.sustavov@cointelegraph.com>
2023-03-03 17:11:41 +0500

    added real-time graph

--- binance.py
+++ binance.py
@@ -1,5 +1,6 @@
 import requests
 import threading
+import matplotlib.pyplot as plt
 from datetime import datetime
 
 
@@ -18,11 +19,36 @@ def get_price(symbol):
         run()
 
 
+def create_plot():
+    global graph
+    global date_time
+    global ax
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    graph = []
+    date_time = []
+
+
+def get_graph(price, date, symbol):
+    graph.append(float(price))
+    date_time.append(date)
+    ax.clear()
+    ax.plot(date_time, graph)
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title(f'{symbol} Price history')
+    plt.ylabel('Price')
+    plt.xlabel('Time')
+    plt.draw()
+    plt.pause(15)
+
+
 def run():
     symbol = get_inputs()
     price = get_price(symbol)
     print(f'Actual {symbol[0]} price for {symbol[1]} at '
           f'({datetime.now().strftime("%Y-%m-%d %H:%M:%S")}):', f'{float(price):,}')
+    create_plot()
 
     def recursion(price):
         new_price = get_price(symbol)
@@ -35,7 +61,8 @@ def run():
                 price = new_price
                 print("\U0001F534", f'Actual {symbol[0]} price for {symbol[1]} at '
                                     f'({datetime.now().strftime("%Y-%m-%d %H:%M:%S")}):', f'{float(price):,}')
-        threading.Timer(30, recursion(price)).start()
+        get_graph(price, datetime.now().strftime("%Y-%m-%d %H:%M:%S"), symbol[0])
+        threading.Timer(15, recursion(price)).start()
     recursion(price)
 
 
